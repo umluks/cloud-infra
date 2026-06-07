cb25c2f5fac8a991efbd2e9e6cd53559c973cfaf
Lucas dos Anjos <lucas.sga@gmail.com>
2026-06-07 14:52:59 -0300

    feat: stages terraform

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,8 +4,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
+  }
 
   backend "s3" {
     bucket       = "terraform-state-sfbjj"
